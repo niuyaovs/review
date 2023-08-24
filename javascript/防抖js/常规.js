9962209abfd944eb30e7d02b1019e946f87e185a
牛垚 <376918909@qq.com>
2023-08-24 09:45:05 +0800

    防抖-常规

---
+++ javascript/防抖js/常规.js
@@ -0,0 +1,14 @@
+// 在delay秒后执行fn，若在n秒内重复触发则重新计时
+function debounce(fn, delay = 500) {
+  let timer;
+  return function () {
+    let context = this;
+    let arg = arguments;
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      timer = null;
+      fn.apply(context, arg);
+    }, delay);
+  };
+}
+export default debounce;
